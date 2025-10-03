855150ae27eb3e5593a11185b4721e38a77bf5aa
NaidelinTorres <nl.torress@uea.edu.ec>
2025-10-03 16:51:38 -0500

    Add files via upload

---
+++ Semana16/src/main/java/com/mycompany/semana16/Semana16.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana16;
+
+/**
+ *
+ * @author USER
+ */
+public class Semana16 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
